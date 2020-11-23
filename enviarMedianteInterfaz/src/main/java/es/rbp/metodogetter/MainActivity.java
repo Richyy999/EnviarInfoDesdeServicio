4cbb8328c37e388e34ff87b578cafa8d40bd0d5b
Ricardo Bordería Pi <rychyy999@gmail.com>
2020-11-23 22:49:03 +0100

    enviar info meniante interfaz

---
+++ enviarMedianteInterfaz/src/main/java/es/rbp/metodogetter/MainActivity.java
@@ -0,0 +1,112 @@
+package es.rbp.metodogetter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Esta activity empieza o termina el {@link ServicioContador} y muestra el segundo actual del servicio
+ *
+ * @author Ricardo Bordería Pi
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, ServiceConnection, ServicioContador.Llamada {
+
+    /**
+     * TextView que muestra el segundo actual del servicio
+     */
+    private TextView lblSegundoActual;
+
+    /**
+     * Instancia del servicio para acceder a sus métodos
+     */
+    private ServicioContador servicio;
+
+    /**
+     * Intent para identificar el servicio
+     */
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnEmpezar = findViewById(R.id.btnEmpezar);
+        Button btnParar = findViewById(R.id.btnParar);
+
+        btnEmpezar.setOnClickListener(this);
+        btnParar.setOnClickListener(this);
+
+        lblSegundoActual = findViewById(R.id.lblSegundoActual);
+
+        intent = new Intent(this, ServicioContador.class);
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i("ACTIVITY", "DESTRUIDO");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btnEmpezar)
+            empezarServicio();
+        else if (id == R.id.btnParar)
+            pararServicio();
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service) {
+        ServicioContador.LocalBinder binder = (ServicioContador.LocalBinder) service;
+        servicio = binder.getServiceInstance();
+        servicio.registrarActivity(MainActivity.this);
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+
+    }
+
+    @Override
+    public void actualizarContador(int segundoActual) {
+        actualizarSegundo(segundoActual);
+    }
+
+    /**
+     * Empieza un {@link ServicioContador} y reinicia el TextView {@link MainActivity#lblSegundoActual}
+     */
+    private void empezarServicio() {
+        lblSegundoActual.setText(String.valueOf(ServicioContador.SEGUNDO_POR_DEFECTO));
+        startService(intent);
+        bindService(intent, this, Context.BIND_AUTO_CREATE);
+    }
+
+    /**
+     * Detiene un {@link ServicioContador}
+     */
+    private void pararServicio() {
+        servicio.pararConteo();
+        unbindService(this);
+        stopService(intent);
+    }
+
+    /**
+     * Muestra el segundo enviado por el servicio y lo muestra en {@link MainActivity#lblSegundoActual}
+     *
+     * @param segundoActual segundo enviao por el servicio
+     */
+    private void actualizarSegundo(int segundoActual) {
+        lblSegundoActual.setText(String.valueOf(segundoActual));
+    }
+}
---
+++ enviarMedianteInterfaz/src/main/java/es/rbp/metodogetter/ServicioContador.java
@@ -0,0 +1,131 @@
+package es.rbp.metodogetter;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Cuenta los segundos que psasn desde que se inció el servicio y los envía a {@link MainActivity}
+ *
+ * @author Ricardo Bordería Pi
+ */
+public class ServicioContador extends Service {
+
+    /**
+     * Extra para el valor por defecto para el intent del contador
+     */
+    public static final int SEGUNDO_POR_DEFECTO = 0;
+
+    /**
+     * Handler para manejar los hilos
+     */
+    private Handler handler;
+
+    /**
+     * Hilo que realiza el conteo y lo envía a las activities que lo escuchen
+     */
+    private Runnable hiloContador;
+
+    private final IBinder binder = new LocalBinder();
+
+    /**
+     * Instancia de {@link Llamada} para acceder a sus métodos y enviar la información
+     */
+    private Llamada llamada;
+
+    /**
+     * Segundo actual desde el inicio del servicio
+     */
+    private int segundoActual;
+
+    /**
+     * Esta clase devuelve la instancia del servicio
+     *
+     * @author Ricardo Bordería Pi
+     */
+    public class LocalBinder extends Binder {
+
+        /**
+         * Devuelve una instancia de {@link ServicioContador}
+         *
+         * @return {@link ServicioContador}
+         */
+        public ServicioContador getServiceInstance() {
+            return ServicioContador.this;
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        segundoActual = 0;
+
+        handler = new Handler();
+
+        hiloContador = new Runnable() {
+            @Override
+            public void run() {
+                segundoActual++;
+                llamada.actualizarContador(segundoActual);
+                handler.postDelayed(hiloContador, 1000);
+            }
+        };
+        handler.postDelayed(hiloContador, 1000);
+        Log.i("SERVICIO", "EMPEZADO");
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(hiloContador);
+        Log.i("SERVICIO", "DESTRUIDO");
+    }
+
+    @Override
+    public boolean stopService(Intent name) {
+        handler.removeCallbacks(hiloContador);
+        Log.i("ESTADO DEL SERVICIO", "PARADO");
+        return super.stopService(name);
+    }
+
+    /**
+     * Detiene el conteo
+     */
+    public void pararConteo() {
+        handler.removeCallbacks(hiloContador);
+        segundoActual = 0;
+    }
+
+    /**
+     * Registra la activity desde la que es llamado
+     *
+     * @param activity activity que implemente {@link Llamada}
+     */
+    public void registrarActivity(Llamada activity) {
+        this.llamada = activity;
+    }
+
+    /**
+     * Interfaz para enviar información desde el servicio hasta el activity que la implemente
+     */
+    public interface Llamada {
+        /**
+         * Envía el segundo actual
+         *
+         * @param segundoActual {@link ServicioContador#segundoActual}
+         */
+        void actualizarContador(int segundoActual);
+    }
+}
